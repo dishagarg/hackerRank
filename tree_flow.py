c059c77cc4dc50dc708d81bbcbb973f7ed62c883
Disha Garg <garg.disha8@gmail.com>
2016-10-09 18:12:43 -0700

    tree_flow.py
    Tree flow problem

---
+++ tree_flow.py
@@ -0,0 +1,32 @@
+
+def dfs(graph, start, n, edge_dist):
+    visited, stack = set(), [start]
+    distances = [-1] * n
+    distances[int(start) -1]=0
+    
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            for v in graph[vertex]:
+                if distances[int(v)-1]==-1:
+                    distances[int(v)-1]=distances[int(vertex)-1]+min(edge_dist[tuple([vertex,v])])
+            stack.extend(graph[vertex] - visited)
+    return filter(lambda x:x!=-1,distances)
+
+n=int(input())  # no. of nodes
+graph=dict()
+edge_dist=dict()
+for r in range(n-1):
+    # taking input from stdin
+    a,b,c=map(int,input().split())
+    # making graph and edge distances
+    graph.setdefault(a,set()).add(b)
+    graph.setdefault(b,set()).add(a)
+    edge_dist.setdefault(tuple([a,b]),list()).append(c)
+    edge_dist.setdefault(tuple([b,a]),list()).append(c)
+# print(graph)
+# print(edge_dist)
+sol = dfs(graph,1,n,edge_dist)
+sol2 = dfs(graph,n,n,edge_dist)
+print(min(sum(sol),sum(sol2)))
