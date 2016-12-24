d717d2b8176a5631abdcf1b2a5912f0db5727bb2
Disha Garg <garg.disha8@gmail.com>
2016-12-23 19:59:41 -0800

    Update camelcase.py

--- camelcase.py
+++ camelcase.py
@@ -1,8 +1,5 @@
-#!/bin/python
-"""
-Given s in CamelCase, print the number of words in s on a new line.
-"""
-import sys
+# -*- coding: utf-8 -*-
+"""Given s in CamelCase, print the number of words in s on a new line."""
 
 s = raw_input().strip()
 print sum(s[i].isupper() for i in range(len(s))) + 1
